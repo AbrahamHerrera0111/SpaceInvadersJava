2eb9c977e270f49cf81df1d8695a39f8f5401b27
AbrahamHerrera0111 <abrahamherrera0111@gmail.com>
2023-11-26 22:53:30 -0600

    Run Graphics 3

--- RunGraphics.java
+++ RunGraphics.java
@@ -39,7 +39,7 @@ public class RunGraphics {
 
     // The main method
     public static void main(String... argv) {
-        System.out.println("holamundo223545");
+        System.out.println("holamundo223545nose");
         //new RunGraphics();
 
     }
